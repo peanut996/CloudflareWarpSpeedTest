1dfddf3c184231a4d192ee8e070b640a3fb7aa98
peanut996 <849421294godw@gmail.com>
2023-10-15 15:20:01 +0800

    [chore] use en as default language.

---
+++ README_CN.md
@@ -0,0 +1,46 @@
+<p align="center">
+    <br> 中文 | <a href="README.md">English</a>
+</p>
+
+# CloudflareWarpSpeedTest
+
+## 简介
+Cloudflare Warp 速度测试是一个命令行工具，用于测试 Cloudflare Warp IP 地址的延迟和速度，并获取关于最低延迟和可用端口的信息。它提供了各种选项，以自定义测试参数并根据特定条件筛选结果。
+
+Inspired(Copied) by [CloudflareSpeedTest](https://github.com/XIU2/CloudflareSpeedTest)
+
+## 安装
+
+  + [Releases](https://github.com/peanut996/CloudflareWarpSpeedTest/releases)
+  + `go install github.com/peanut996/CloudflareWarpSpeedTest@latest` (go version <= 1.20)
+
+
+## 用法
+
+要使用 Cloudflare Warp Speed Test，您可以运行以下命令行选项
+
+```bash
+CloudflareWarpSpeedTest -n 200 -t 10 -tl 300 -q -tll 0 -tlr 0.2 -sl 5 -p 10 -f ip.txt -ip 1.1.1.1 -o result.csv -full
+```
+
+以下是主要可用选项的解释：
+
+  + `-n`    200：指定延迟测试线程的数量。增加此值可以加快延迟测试过程，但不适合性能较低的设备，如路由器。默认值为 200，最大为 1000。
+  + `-t`    10：设置对每个 IP 地址执行延迟测试的次数。默认值为 10 次。
+  + `-q`    快速模式：快速扫描5000个地址结果。**默认打开**， 使用`-q=false` 关闭快速模式。
+  + `-o`    result.csv：设置输出结果文件。默认文件为 "result.csv"。
+  + `-full` 此标志表示应测试指定范围内的所有端口。
+  + `-pri`  自定义wireguard的私钥。
+  + `-pub`  自定义wireguard的公钥。默认为Warp的公钥
+
+更多使用说明请使用`-h`。
+
+## 注意
+
+请注意，调整测试参数可能会影响测试速度和结果。根据设备的性能和您希望应用的特定条件选择合适的设置至关重要。
+
+**免责声明**： 本工具与 Cloudflare 无关，也未得到其认可。请负责任地使用并遵守其服务条款。
+
+## License
+
+此软件根据 [GPL v3 许可证](LICENSE) 发布
--- main.go
+++ main.go
@@ -3,76 +3,88 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/peanut996/CloudflareWarpSpeedTest/task"
 	"github.com/peanut996/CloudflareWarpSpeedTest/utils"
 )
 
+var (
+	Version string
+)
+
 func init() {
 	var printVersion bool
 	var help = `
-CloudflareWarpSpeedTest \n` + `
-测试 Cloudflare Warp 所有 IP 的延迟和速度，获取最低延迟和端口
+CloudflareWarpSpeedTest ` + Version + `\n` + `
+Test the latency and speed of all Cloudflare Warp IPs to obtain the lowest latency and port.
 
-参数：
+Parameters:
     -n 200
-        延迟测速线程；越多延迟测速越快，性能弱的设备 (如路由器) 请勿太高；(默认 200 最多 1000)
+        Latency test threads; the more threads, the faster the latency test, but do not set it too high on low-performance devices (such as routers); (default 200, maximum 1000)
     -t 10
-        延迟测速次数；单个 IP 延迟测速的次数；(默认 10 次)
-    -q 
-        快速模式； 随机扫描5000的地址测速结果；默认打开，[-q=false] 关闭快速模式
+        Number of latency tests; the number of times to test latency for a single IP; (default 10 times)
+    -q
+        Quick mode; test results for randomly scanning 5000 addresses; on by default, [-q=false] turns off quick mode
     -tl 300
-        平均延迟上限；只输出低于指定平均延迟的 IP，各上下限条件可搭配使用；(默认 300 ms)
+        Average latency upper limit; only output IPs with average latency lower than the specified limit, various upper and lower limit conditions can be used together; (default 300 ms)
     -tll 40
-        平均延迟下限；只输出高于指定平均延迟的 IP；(默认 0 ms)
+        Average latency lower limit; only output IPs with average latency higher than the specified limit; (default 0 ms)
     -tlr 0.2
-        丢包几率上限；只输出低于/等于指定丢包率的 IP，范围 0.00~1.00，0 过滤掉任何丢包的 IP；(默认 1.00)
+        Packet loss rate upper limit; only output IPs with packet loss rate lower than or equal to the specified rate, range 0.00~1.00, 0 filters out any IPs with packet loss; (default 1.00)
     -sl 5
-        下载速度下限；只输出高于指定下载速度的 IP，凑够指定数量 [-dn] 才会停止测速；(默认 0.00 MB/s)
-
+        Download speed lower limit; only output IPs with download speed higher than the specified speed, testing stops when reaching the specified quantity [-dn]; (default 0.00 MB/s)
     -p 10
-        显示结果数量；测速后直接显示指定数量的结果，为 0 时不显示结果直接退出；(默认 10 个)
+        Number of results to display; directly display the specified number of results after testing, 0 means not displaying results and exiting directly; (default 10)
     -f ip.txt
-        IP段数据文件；如路径含有空格请加上引号；
+        IP segment data file; add quotes if the path contains spaces;
     -ip 1.1.1.1,2.2.2.2/24,2606:4700::/32
-        指定IP段数据；直接通过参数指定要测速的 IP 段数据，英文逗号分隔；(默认 空)
+        Specify IP segment data; directly specify the IP segment data to be tested through parameters, separated by commas; (default empty)
     -o result.csv
-        写入结果文件；如路径含有空格请加上引号；值为空时不写入文件 [-o ""]；(默认 result.csv)
-    -pri 私钥
-        指定你的wireguard私钥
-    -pub 私钥
-        指定你的wireguard公钥, 默认为warp的公钥
+        Write result to file; add quotes if the path contains spaces; empty value means not writing to a file [-o ""]; (default result.csv)
+    -pri PrivateKey
+        Specify your WireGuard private key
+    -pub PublicKey
+        Specify your WireGuard public key, default is the Warp public key
     -full
-        测速全部的端口；对 IP 段中的每个 IP 全部端口进行测速
+        Test all ports; test all ports for each IP in the IP segment
     -h
-        打印帮助说明
+        Print the help explanation
+    -v 
+        Print the version and build time
 `
+
 	var minDelay, maxDelay int
 	var maxLossRate float64
-	flag.IntVar(&task.Routines, "n", 200, "延迟测速线程")
-	flag.IntVar(&task.PingTimes, "t", 10, "延迟测速次数")
+	flag.IntVar(&task.Routines, "n", 200, "Latency test threads")
+	flag.IntVar(&task.PingTimes, "t", 10, "Number of latency test times")
 
-	flag.IntVar(&maxDelay, "tl", 300, "平均延迟上限")
-	flag.IntVar(&minDelay, "tll", 0, "平均延迟下限")
-	flag.Float64Var(&maxLossRate, "tlr", 1, "丢包几率上限")
+	flag.IntVar(&maxDelay, "tl", 300, "Average latency upper limit")
+	flag.IntVar(&minDelay, "tll", 0, "Average latency lower limit")
+	flag.Float64Var(&maxLossRate, "tlr", 1, "Packet loss rate upper limit")
 
-	flag.BoolVar(&task.ScanAllPort, "full", false, "扫描全部端口")
-	flag.BoolVar(&task.QuickMode, "q", true, "快速模式，仅随机扫描5000个IP的结果")
-	flag.IntVar(&utils.PrintNum, "p", 10, "显示结果数量")
-	flag.StringVar(&task.IPFile, "f", "", "IP段数据文件")
-	flag.StringVar(&task.IPText, "ip", "", "指定IP段数据")
-	flag.StringVar(&utils.Output, "o", "result.csv", "输出结果文件")
-	flag.StringVar(&task.PrivateKey, "pri", "", "指定private key")
-	flag.StringVar(&task.PrivateKey, "pub", "", "指定public key")
+	flag.BoolVar(&task.ScanAllPort, "full", false, "Scan all ports")
+	flag.BoolVar(&task.QuickMode, "q", true, "Quick mode, test results for randomly scanning 5000 IPs")
+	flag.IntVar(&utils.PrintNum, "p", 10, "Number of results to display")
+	flag.StringVar(&task.IPFile, "f", "", "IP segment data file")
+	flag.StringVar(&task.IPText, "ip", "", "Specify IP segment data")
+	flag.StringVar(&utils.Output, "o", "result.csv", "Output result file")
+	flag.StringVar(&task.PrivateKey, "pri", "", "Specify private key")
+	flag.StringVar(&task.PrivateKey, "pub", "", "Specify public key")
+	flag.BoolVar(&printVersion, "v", false, "Print program version")
 
-	flag.BoolVar(&printVersion, "v", false, "打印程序版本")
 	flag.Usage = func() { fmt.Print(help) }
 	flag.Parse()
 
 	utils.InputMaxDelay = time.Duration(maxDelay) * time.Millisecond
 	utils.InputMinDelay = time.Duration(minDelay) * time.Millisecond
 	utils.InputMaxLossRate = float32(maxLossRate)
+
+	if printVersion {
+		fmt.Println(Version)
+		os.Exit(0)
+	}
 }
 
 func main() {
